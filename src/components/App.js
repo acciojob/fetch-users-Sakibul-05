62ac0437074d3929eb2a7ebebc47555b70382e2f
Sakibul <sksakibulislam30@gmail.com>
2024-02-09 20:24:01 +0530

    submitting code-Fri Feb 09 2024 20:23:58 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,74 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [error, setError] = useState("");
+
+  function getUser() {
+    fetch("https://reqres.in/api/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsDisabled((pre) => !pre);
+        console.log(data.data);
+        setUsers(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.message);
+      });
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div className="companyNameAndGetUserBtn">
+        <h2>Blue Whales</h2>
+        <button className="btn" disabled={isDisabled} onClick={() => getUser()}>
+          Get User List
+        </button>
+      </div>
+      <div className="userListContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Avatar</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length === 0 ? (
+              <>
+                <tr>
+                  <td colSpan="4" style={{ border: "none", padding: 0 }}>
+                    <p className="messege">
+                      {error ? error : "No data found to display"}
+                    </p>
+                  </td>
+                </tr>
+              </>
+            ) : (
+              <>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.first_name}</td>
+                    <td>{user.last_name}</td>
+                    <td>{user.email}</td>
+                    <td>
+                      <img src={user.avatar}></img>
+                    </td>
+                  </tr>
+                ))}
+              </>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
